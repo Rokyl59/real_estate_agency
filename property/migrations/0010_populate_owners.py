c61f835e0a52d5b24744f4e0da02c1815702f393
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-05-05 13:15:34 +0500

    Высушил код

--- property/migrations/0010_populate_owners.py
+++ property/migrations/0010_populate_owners.py
@@ -7,21 +7,19 @@ def create_owners(apps, schema_editor):
     Owner = apps.get_model('property', 'Owner')
     for flat in Flat.objects.all().iterator():
         phone_number = flat.owners_phonenumber
-        try:
-            parsed_phone = phonenumbers.parse(phone_number, 'RU') if phone_number else None
-            if parsed_phone and phonenumbers.is_valid_number(parsed_phone):
-                normalized_phone = phonenumbers.format_number(parsed_phone, phonenumbers.PhoneNumberFormat.E164)
-            else:
-                normalized_phone = None
-            owner, created = Owner.objects.get_or_create(
-                name=flat.owner,
-                defaults={'phonenumber': phone_number, 'owner_pure_phone': normalized_phone}
-            )
-            owner.flats.add(flat)
-        except phonenumbers.NumberParseException:
-            continue
-        except ValueError:
-            continue
+        normalized_phone = None
+        if phone_number:
+            try:
+                parsed_phone = phonenumbers.parse(phone_number, 'RU')
+                if phonenumbers.is_valid_number(parsed_phone):
+                    normalized_phone = phonenumbers.format_number(parsed_phone, phonenumbers.PhoneNumberFormat.E164)
+            except phonenumbers.NumberParseException:
+                pass
+        owner, created = Owner.objects.get_or_create(
+            name=flat.owner,
+            defaults={'phonenumber': phone_number, 'owner_pure_phone': normalized_phone}
+        )
+        owner.flats.add(flat)
 
 
 class Migration(migrations.Migration):
